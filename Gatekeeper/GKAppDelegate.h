839c50e8be5be66df4ee04bf4a8e652692085bf9
Sean McGary <sean.mcgary@gmail.com>
2012-04-08 22:58:07 -0400

    Initial Commit

---
+++ Gatekeeper/GKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GKAppDelegate.h
+//  Gatekeeper
+//
+//  Created by Sean McGary on 4/8/12.
+//  Copyright (c) 2012 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gatekeeper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gatekeeper
+//
+//  Created by Sean McGary on 4/8/12.
+//  Copyright (c) 2012 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GKAppDelegate class]));
+    }
+}
